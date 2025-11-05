751126d206990fd26a160af55cb0c658077d4154
CakuDev <cakudev@gmail.com>
2025-11-05 19:56:09 +0100

    [#10] Add basic abstract class for interaction functionality

---
+++ Assets/Scripts/Interaction/InteractiveBehaviour.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public abstract class InteractiveBaseBehaviour
+{
+    public void OnPlayerEntered()
+    {
+        //TODO: Add common functionality
+    }
+
+    public void OnPlayerExited()
+    {
+        //TODO: Add common functionality
+    }
+
+    public abstract void Interact();
+}
