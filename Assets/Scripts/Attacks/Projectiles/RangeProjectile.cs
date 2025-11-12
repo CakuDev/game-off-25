5fc36a7c6cb20f9c9bf958d36f104f62ca406a31
CakuDev <cakudev@gmail.com>
2025-11-12 15:18:06 +0100

    [#27] Add basic attack feature

---
+++ Assets/Scripts/Attacks/Projectiles/RangeProjectile.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class RangeProjectile : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // TODO: Check if it's a wall or has a health component 
+    }
+}
---
+++ Assets/Scripts/Attacks/RangeAttackBehaviour.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class RangeAttackBehaviour : MonoBehaviour, IAttackBehaviour
+{
+
+    public void Attack()
+    {
+        Debug.Log("Range attack!");
+    }
+}
